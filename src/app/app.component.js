802cf66d0b9a6eba2d586537424813d3c4119b7b
antarus66 <andriy.tarusin@binary-studio.com>
2016-10-24 21:40:02 +0300

    Added a component with todos adding and removing.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: "my-app",
-	template: '<h1>My First Angular 2 App</h1>'
+    selector: "todo-app",
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
 
 export class AppComponent {
-  constructor(){}
+    constructor() {
+       this.title = "Todo list";
+       this.todos = new Set();
+    };
+
+    addTodo(input) {
+        let title = input.value;
+        input.value = '';
+        this.todos.add({
+            title,
+            checked: false,
+
+            get done() {
+                return this.checked;
+            },
+
+            set done(val) {
+                if (val) {
+                    alert('Cool!')
+                } else {
+                    alert('Fool!');
+                }
+
+                this.checked = val;
+            }
+        });
+    }
+
+    removeTodo(todo) {
+        this.todos.delete(todo);
+    }
 }
