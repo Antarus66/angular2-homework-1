69ccfb1bb36bac29b16aa5323d1c549d3365f4c7
antarus66 <andriy.tarusin@binary-studio.com>
2016-10-25 20:17:00 +0300

    Added the todo-form compoment without functionality

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component.js';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoFormComponent } from './todo/todo-form/todo-form.component';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoFormComponent
   ],
   imports: [ BrowserModule ]
 })
